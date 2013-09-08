2773ba764129631b641bc9ba0e3bb5cdef57b3ff
thmsLGthomas.leguillou@gmail.com <thmsLGthomas.leguillou@gmail.com>
2013-09-08 03:51:21 -0700

    add "write" op to ask measurement on channel 1

--- main.cpp
+++ main.cpp
@@ -41,21 +41,25 @@ int main()
 	    exit(1);
 	}
 
+	//--------------------------------------------------------------------------------------------------------
 	char buf[10] = {0};
+	buf[0] = 0x9C;
+	if (write(file,buf,1) != 1) {
+	    /* ERROR HANDLING: i2c transaction failed */
+	    printf("Failed to write to the i2c bus.\n");
+	    printf("\n\n");
+	}
+
+	//--------------------------------------------------------------------------------------------------------
+
 	float data;
 	char channel;
 
-	for (int i = 0; i<4; i++) {
-	    // Using I2C Read
-	    if (read(file,buf,2) != 2) {
-	        /* ERROR HANDLING: i2c transaction failed */
-	        printf("Failed to read from the i2c bus.\n");
-	        printf("\n\n");
-	    } else {
-	        data = (float)((buf[0] & 0b00001111)<<8)+buf[1];
-	        data = data/4096*5;
-	        channel = ((buf[0] & 0b00110000)>>4);
-	        printf("Channel %02d Data:  %04f\n",channel,data);
-	    }
+	// Using I2C Read
+	if (read(file,buf,2) != 2) {
+		/* ERROR HANDLING: i2c transaction failed */
+		printf("Failed to read from the i2c bus.\n");
+	} else {
+		printf("Read was successful");
 	}
 }
